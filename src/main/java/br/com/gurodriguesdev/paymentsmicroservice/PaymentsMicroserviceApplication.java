fe1e4261c4e10f2440af61922bfbe670782371c4
gurodrigues-dev <gustavorodrigueslima2004@gmail.com>
2024-04-08 23:06:30 -0300

    init commit

---
+++ src/main/java/br/com/gurodriguesdev/paymentsmicroservice/PaymentsMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package br.com.gurodriguesdev.paymentsmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentsMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentsMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gurodriguesdev/paymentsmicroservice/PaymentsMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gurodriguesdev.paymentsmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentsMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
